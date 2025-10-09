9ab0dc5019f5cf8719d4646d46ab52fc97b7ca29
volenteremmy-ux <volenteremmy@gmail.com>
2025-10-09 12:12:53 +0300

    Day 6 Update

---
+++ Digitech Python/Day6.py
@@ -0,0 +1,22 @@
+# a = 5
+# b = 4
+# calc = a & b
+# print(calc)
+
+# a = 5
+# b = 4
+# calc = a & b
+# binary = bin(calc)
+# print(f"{calc} and is a binary {binary}")
+
+# a = 8
+# b = 6
+# calc = a | b
+# binary = bin(calc)
+# print(f"{calc} and is a binary {binary}")
+
+a = 8
+b = 10
+calc = a ^ b
+binary = bin(calc)
+print(f"{calc} and is a binary {binary}")
